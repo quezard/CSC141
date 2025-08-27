6ebfdb18cac34fa390e99c5085327fbd5d18f979
marquez <marquez.thomas001@albright.edu>
2025-08-27 14:10:24 -0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+1-2 when an typo is made the software will say type error and tell you hich line of code. You will not recive a typo error after a typo if your occors in prenthesies or a non command.
+1-3 by the end of the semester my goal is to be refreshed on how to make websites and hopefully get better ant it as well as make smoother games.
