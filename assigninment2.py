b3480e8ac9c9943644f76055561f546bb0ece3f6
marquez <marquez.thomas001@albright.edu>
2025-09-03 09:30:00 -0400

    Add files via upload

---
+++ assigninment2.py
@@ -0,0 +1,35 @@
+message="hey quez, would you like to learn python."
+print(message)
+first_name="marquez"
+last_name="thomas"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+name="marquez"
+print(name.lower())    
+print(name.upper())    
+print(name.title())
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, “A person who never made a mistake never tried anything new.”'
+print(message)
+name_with_whitespace = "\t\n  Marquez Thomas  \n\t"
+print("Original with whitespace:")
+print(name_with_whitespace)
+
+print("\nUsing lstrip():")
+print(name_with_whitespace.lstrip())
+
+print("\nUsing rstrip():")
+print(name_with_whitespace.rstrip())
+
+print("\nUsing strip():")
+print(name_with_whitespace.strip())
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))
+print(5 + 3)   #addition
+print(10 - 2)  #subtraction
+print(4 * 2)   
+print(16 / 2)  
+favorite_number = 7
+message = f"My favorite number is {favorite_number}."
+print(message)
+import this
